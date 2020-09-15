9222ae375c679f3230df64bea0b901adbf7132c4
Nikolay Kormushev <nkormushev@gmail.com>
2020-09-15 18:07:08 +0300

    Made an error on purpose to test failing pipeline

--- hello_world.go
+++ hello_world.go
@@ -9,7 +9,7 @@ import (
 func PrintHelloWorld(w io.Writer) error {
 	_, err := w.Write([]byte("Hello, world!"))
 
-	if err != nil {
+	if err = nil {
 		return fmt.Errorf("Failed to write hello world")
 	}
 
